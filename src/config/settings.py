13a682f37eb92c049d3936a52d7f6340d8cb5c3d
Christie Entwistle <ceoptimize@gmail.com>
2025-08-28 11:59:07 -0400

    fixed docker compose

--- src/config/settings.py
+++ src/config/settings.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     )
 
     # Neo4j Connection Settings
-    neo4j_uri: str = "bolt://localhost:7687"
+    neo4j_uri: str = "bolt://localhost:7688"
     neo4j_user: str = "neo4j"
     neo4j_password: str = "password"
     neo4j_database: str = "neo4j"
